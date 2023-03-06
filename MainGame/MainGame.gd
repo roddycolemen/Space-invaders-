76c111fd27f12ebb3b61963e637814ab0bcdd826
0384359 <124739251+roddycolemen@users.noreply.github.com>
2023-03-06 14:44:22 +1100

    ok

--- MainGame/MainGame.gd
+++ MainGame/MainGame.gd
@@ -7,3 +7,7 @@ func _ready():
 	pass 
 	currentTimer = countdownMax
 	$HUD/Countdown.text = str(currentTimer)
+	while currentTimer > 0:
+		yield(get_tree().create_timer(1.0), "timeout")
+		currentTimer = currentTimer - 1
+		print(currentTimer)
